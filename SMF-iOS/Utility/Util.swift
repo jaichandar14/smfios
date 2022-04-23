f6eb6092855249c1094dae1f58c9fc8f516a325f
SwapnilDhotre <swapnildhotre09@gmail.com>
2022-04-23 22:17:22 +0530

    2521 Git initial commit

---
+++ SMF-iOS/Utility/Util.swift
@@ -0,0 +1,27 @@
+//
+//  Util.swift
+//  SMF-iOS
+//
+//  Created by Swapnil_Dhotre on 4/23/22.
+//
+
+import UIKit
+
+class Util {
+    
+    static func setIntialController(window: UIWindow) {
+        
+        window.rootViewController = UINavigationController(rootViewController: InitialViewController())
+        window.makeKeyAndVisible()
+    }
+    
+    static func isBackPressed(string: String) -> Bool {
+        let char = string.cString(using: String.Encoding.utf8)
+        let isBackSpace: Int = Int(strcmp(char, "\u{8}"))
+        if isBackSpace == -8 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
